bd6f6bb5bd03bd19c53f0825b21ac3e048c1b98c
dsrlem <darlem_dbs@hotmail.com>
2024-05-24 00:09:43 -0300

    harmonia

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
-import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import dj_database_url
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -14,33 +13,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-def get_db():
-    conn = sqlite3.connect('library.db')
-    return conn
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    edition_id = db.Column(db.Integer)
 
 @app.route('/')
 def index():
     search_query = request.args.get('search')
-    conn = get_db()
-    cur = conn.cursor()
     if search_query:
-        cur.execute("""
-            SELECT * FROM books 
-            WHERE id LIKE ? 
-            OR title LIKE ? 
-            OR author LIKE ? 
-            OR genre LIKE ? 
-            OR year LIKE ?""",
-                    ('%' + search_query + '%',
-                     '%' + search_query + '%',
-                     '%' + search_query + '%',
-                     '%' + search_query + '%',
-                     '%' + search_query + '%'))
+        books = Book.query.filter(
+            db.or_(
+                Book.id.like('%{}%'.format(search_query)),
+                Book.title.like('%{}%'.format(search_query)),
+                Book.author.like('%{}%'.format(search_query)),
+                Book.genre.like('%{}%'.format(search_query)),
+                Book.year.like('%{}%'.format(search_query))
+            )
+        ).all()
     else:
-        cur.execute('SELECT * FROM books')
-    books = cur.fetchall()
-    conn.close()
+        books = Book.query.all()
     return render_template('index.html', books=books, search_query=search_query)
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -57,19 +54,15 @@ def add_book():
             error = "Ano deve ser um número."
             return render_template('add_book.html', error=error)
 
-        conn = get_db()
-        cur = conn.cursor()
-        cur.execute('INSERT INTO books (title, author, genre, year, edition_id) VALUES (?, ?, ?, ?, ?)',
-                    (title, author, genre, year, edition_id))
-        conn.commit()
-        conn.close()
+        book = Book(title=title, author=author, genre=genre, year=year, edition_id=edition_id)
+        db.session.add(book)
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('add_book.html')
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_book(id):
-    conn = get_db()
-    cur = conn.cursor()
+    book = Book.query.get_or_404(id)
     if request.method == 'POST':
         title = request.form['title']
         author = request.form['author']
@@ -79,28 +72,23 @@ def edit_book(id):
 
         if not year.isdigit():
             error = "Ano deve ser um número."
-            cur.execute('SELECT * FROM books WHERE id = ?', (id,))
-            book = cur.fetchone()
-            conn.close()
             return render_template('edit_book.html', book=book, error=error)
 
-        cur.execute('UPDATE books SET title = ?, author = ?, genre = ?, year = ?, edition_id = ? WHERE id = ?',
-                    (title, author, genre, year, edition_id, id))
-        conn.commit()
-        conn.close()
+        book.title = title
+        book.author = author
+        book.genre = genre
+        book.year = year
+        book.edition_id = edition_id
+
+        db.session.commit()
         return redirect(url_for('index'))
-    cur.execute('SELECT * FROM books WHERE id = ?', (id,))
-    book = cur.fetchone()
-    conn.close()
     return render_template('edit_book.html', book=book)
 
 @app.route('/delete/<int:id>')
 def delete_book(id):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute('DELETE FROM books WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
